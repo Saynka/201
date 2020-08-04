0425da4d69b997e3a0e63069c87c33da8473c6d1
kale lesko <sankakale@gmail.com>
2020-08-04 15:23:06 -0700

    lab 2

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict'; 
+
+var questionOne = prompt('What city was i born in?')
+if (questionOne.toLowerCase() === 'pittsburgh'){
+  console.log ("got question currect")
+    alert('The Iron city!')
+  document.write('For question one you answered ' + questionOne + '. The Iron city!')
+} else {
+  alert('Close...')
+  document.write('For question one you answered ' + questionOne + '. Close...')
+}
+
+var questionTwo = prompt('the rainest city in north america?')
+if (questionTwo.toLowerCase() === 'ketchikan'){
+  console.log ("got question currect")
+    alert('The First city')
+  document.write('For question one you answered ' + questionTwo + '. The First city')
+} else {
+  alert('Close...')
+  document.write('For question one you answered ' + questionTwo + '. Close...')
+}
+
+var questionThree = prompt('when did i move to Phoenix?')
+if (questionThree.toLowerCase() === '18'){
+  console.log ("got question currect")
+    alert('The valley of the sun')
+  document.write('For question one you answered ' + questionThree + '. The valley of the sun')
+} else {
+  alert('Close...')
+  document.write('For question one you answered ' + questionThree + '. Close...')
+}
+
+var questionFour = prompt('why did i move to anchorage')
+if (questionFour.toLowerCase() === 'college'){
+  console.log ("got question currect")
+    alert('36 Crazyfists')
+  document.write('For question one you answered ' + questionFour + '. Bitterness the star')
+} else {
+  alert('Close...')
+  document.write('For question one you answered ' + questionFour + '. Close...')
+}
+
+var questionFive = prompt('my brother and me did what?')
+if (questionFive.toLowerCase() === 'catering'){
+  console.log ("got question currect")
+    alert('...hippies')
+  document.write('For question one you answered ' + questionFive + '. Pele is a angry god')
+} else {
+  alert('Close...')
+  document.write('For question one you answered ' + questionFive + '. Close...')
+}
