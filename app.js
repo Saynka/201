37610662cc03ecbd17e3188fc36c4fc5f0d8693e
kale lesko <sankakale@gmail.com>
2020-08-05 21:36:11 -0700

    lab4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'; 
 
-var questionOne = prompt('What city was i born in?')
+var questionOne = prompt('What city was i born in? pittsburgh or utah')
 if (questionOne.toLowerCase() === 'pittsburgh'){
   console.log ("got question currect")
     alert('The Iron city!')
@@ -10,7 +10,7 @@ if (questionOne.toLowerCase() === 'pittsburgh'){
   document.write('For question one you answered ' + questionOne + '. Close...')
 }
 
-var questionTwo = prompt('the rainest city in north america?')
+var questionTwo = prompt('the rainest city in north america? hilo or ketchikan')
 if (questionTwo.toLowerCase() === 'ketchikan'){
   console.log ("got question currect")
     alert('The First city')
@@ -20,7 +20,7 @@ if (questionTwo.toLowerCase() === 'ketchikan'){
   document.write('For question one you answered ' + questionTwo + '. Close...')
 }
 
-var questionThree = prompt('when did i move to Phoenix?')
+var questionThree = prompt('when did i move to Phoenix? 16 or 18?')
 if (questionThree.toLowerCase() === '18'){
   console.log ("got question currect")
     alert('The valley of the sun')
@@ -49,3 +49,57 @@ if (questionFive.toLowerCase() === 'catering'){
   alert('Close...')
   document.write('For question one you answered ' + questionFive + '. Close...')
 }
+
+// var wannaPlay = prompt('Do you want to play a gussing game, enter 1, if not enter 2');
+  
+
+var correctAnswer = 12;
+
+var guest = 0
+
+
+while(guest < 4){
+  var userAnswer = prompt('Please enter a number between 1 and 20');
+        if(parseInt(userAnswer) === correctAnswer){
+            alert('DING DING DING.  You got it right. 1 for yes, 2 for no');
+            break;
+        } else if(guest === 3 && parseInt(userAnswer) !== correctAnswer){  
+            alert('times up neo')
+            break;
+           
+         } else if (parseInt(userAnswer) < correctAnswer){
+             guest++  
+          alert('Incorrect, to low.  Try Again.');
+            } else {
+             guest++ 
+          alert('Incorrect, to high.  Try Again.');
+            }
+        }
+
+
+var correctAnswer2 = ['11,12,13'];
+
+var guess = 0
+        
+while(guess < 7){
+   var userAnswer = prompt('Please enter a number between 1 and 20');
+        if(parseInt(userAnswer) === correctAnswer2){
+            alert('DING DING DING.  You got it right. 1 for yes, 2 for no');
+              break;
+                } else if(guess === 6 && parseInt(userAnswer) !== correctAnswer2){  
+                    alert('11...12....13')
+                    break;
+                   
+                 } else if (parseInt(userAnswer) < correctAnswer2){
+                     guess++ 
+                  alert('Incorrect, to low.  Try Again.');
+                    } else {
+                     guess++ 
+                  alert('Incorrect, to high.  Try Again.');
+                    }
+                }
+        
+
+    
+
+
