d284cc5703201acbc19a1d2639b1e994632ce8a3
kale lesko <sankakale@gmail.com>
2020-08-10 13:53:22 -0700

    labsdone

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict'; 
 
+var score = 0
+
 function name() {
   var userName = prompt('What is your name?');
 // console.log('the user\'s name is' + userName);
@@ -14,11 +16,13 @@ var questionOne = prompt('What city was i born in? pittsburgh or utah')
 if (questionOne.toLowerCase() === 'pittsburgh'){
   console.log ("got question currect")
     alert('The Iron city!')
+    score = score+1;
   document.write('For question one you answered ' + questionOne + '. The Iron city!')
 } else {
   alert('Close...')
   document.write('For question one you answered ' + questionOne + '. Close...')
 }
+
 }
 
 
@@ -27,6 +31,7 @@ var questionTwo = prompt('the rainest city in north america? hilo or ketchikan')
 if (questionTwo.toLowerCase() === 'ketchikan'){
   console.log ("got question currect")
     alert('The First city')
+    score = score+1;
   document.write('For question one you answered ' + questionTwo + '. The First city')
 } else {
   alert('Close...')
@@ -39,6 +44,7 @@ var questionThree = prompt('when did i move to Phoenix? 16 or 18?')
 if (questionThree.toLowerCase() === '18'){
   console.log ("got question currect")
     alert('The valley of the sun')
+    score = score+1;
   document.write('For question one you answered ' + questionThree + '. The valley of the sun')
 } else {
   alert('Close...')
@@ -51,6 +57,7 @@ var questionFour = prompt('why did i move to anchorage')
 if (questionFour.toLowerCase() === 'college'){
   console.log ("got question currect")
     alert('36 Crazyfists')
+    score = score+1;
   document.write('For question one you answered ' + questionFour + '. Bitterness the star')
 } else {
   alert('Close...')
@@ -63,6 +70,7 @@ var questionFive = prompt('my brother and me did what?')
 if (questionFive.toLowerCase() === 'catering'){
   console.log ("got question currect")
     alert('...hippies')
+    score = score+1;
   document.write('For question one you answered ' + questionFive + '. Pele is a angry god')
 } else {
   alert('Close...')
@@ -83,6 +91,7 @@ while(guest < 4){
       console.log('the Guest' + guest);
         if(parseInt(userAnswer) === correctAnswer){
             alert('DING DING DING.  You got it right');
+            score = score+1;
             break;
         } else if(guest === 3 && parseInt(userAnswer) !== correctAnswer){  
             alert('times up neo')
@@ -108,7 +117,10 @@ while(guess < 6){
    var userAnswer = prompt('Please enter a number between 1 and 20');
    console.log('the Guest' + guess);
         if(parseInt(userAnswer) === correctAnswer2[0] || parseInt(userAnswer) === correctAnswer2[1] || parseInt(userAnswer) === correctAnswer2[2]){
-            alert('DING DING DING.  You got it right. 11, 12, 13' + userName);
+            alert('DING DING DING.  You got it right. 11, 12, 13');
+            score = score+1;
+            console.log(score)
+            alert('you got a final score of ' +  score);
               break;
                 } else if(guess === 5 && parseInt(userAnswer) !== correctAnswer2){  
                     alert('11...12....13')
@@ -124,15 +136,18 @@ while(guess < 6){
                 }
         
               }
-                
+     
               
-                // name();
-                // alpha();
-                // bubba();
-                // jlk();
-                // boba();
-                // lelo();
-                // question6();
+              
+                name();
+                alpha();
+                bubba();
+                jlk();
+                boba();
+                lelo();
+                question6();
                 big2();
+
+                
 
 
